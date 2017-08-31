990d752bdc74d8f078bd0eb4df799abbe32231e8
zoecream <zoecream@163.com>
2017-08-31 15:32:21 +0800

    初步完成xml模块并测试通过.

--- platform/src/mod/xml.c
+++ platform/src/mod/xml.c
@@ -9,31 +9,13 @@
 
 #include <xml.h>
 
-#define cxmlStr 1
-#define cxmlObj 2
-
-#define mxmlSkip(data) *data+=strspn(*data,"\t\v\n\r\f ")
-
-/*========================================*\
-    功能 : 导出所有类型节点
-    参数 : (输入)数据结构节点
-           (输出)起始结束位置
-    返回 : (成功)0
-           (失败)-1
-\*========================================*/
-int fxmlAllExport(struct txmlItem *item,char **dst);
-/*========================================*\
-    功能 : 导入所有类型节点
-    参数 : (输入)数据结构节点
-           (出入)起始结束位置
-    返回 : (成功)0
-           (失败)-1
-\*========================================*/
-int fxmlAllImport(struct txmlItem *item,char **src);
+#define cxmlSpace "\t\v\n\r\f "
+#define mxmlSkip(data,accept) *data+=strspn(*data,accept)
+#define mxmlSize(data,reject) strcspn(*data,reject)
 
 /*========================================*\
-    功能 : 数据结构节点分配
-    参数 : (输出)数据结构节点
+    功能 : 节点分配
+    参数 : (输出)节点
     返回 : (成功)0
            (失败)-1
 \*========================================*/
@@ -47,77 +29,84 @@ int fxmlInit(struct txmlItem **item)
 }
 
 /*========================================*\
-    功能 : 数据结构节点释放
-    参数 : (输入)数据结构节点
+    功能 : 节点释放
+    参数 : (输入)节点
     返回 : 空
 \*========================================*/
 void fxmlFree(struct txmlItem *item)
 {
-	if(item->chld)
-		fxmlFree(item->chld);
+	if(item->nodechld)
+		fxmlFree(item->nodechld);
+	if(item->attrchld)
+		fxmlFree(item->attrchld);
 	if(item->next)
 		fxmlFree(item->next);
+	if(item->keyd)
+		free(item->keyd);
 	if(item->vald)
 		free(item->vald);
 	free(item);
 }
 
 /*========================================*\
-    功能 : 创建内容类型节点
-    参数 : (输出)数据结构节点
+    功能 : 创建节点
+    参数 : (输出)节点
+           (输入)类型
+           (输入)键数据
+           (输入)键长度
            (输入)值数据
            (输入)值长度
     返回 : (成功)0
            (失败)-1
 \*========================================*/
-int fxmlValCreate(struct txmlItem **item,char *vald,int vall)
+int fxmlCreate(struct txmlItem **item,int type,char *keyd,int keyl,char *vald,int vall)
 {
 	int result;
 	result=fxmlInit(item);
 	if(result!=0)
 		return -1;
-	(*item)->type=cxmlVal;
-	(*item)->vald=(char*)malloc(vall);
-	if((*item)->vald==NULL)
-		return -1;
-	memcpy((*item)->vald,vald,vall);
-	(*item)->vall=vall;
+	(*item)->type=type;
+	if(keyd!=NULL&&keyl!=0)
+	{
+		(*item)->keyd=(char*)malloc(keyl);
+		if((*item)->keyd==NULL)
+			return -1;
+		memcpy((*item)->keyd,keyd,keyl);
+		(*item)->keyl=keyl;
+	}
+	if(vald!=NULL&&vall!=0)
+	{
+		(*item)->vald=(char*)malloc(vall);
+		if((*item)->vald==NULL)
+			return -1;
+		memcpy((*item)->vald,vald,vall);
+		(*item)->vall=vall;
+	}
 	return 0;
 }
 
 /*========================================*\
-    功能 : 创建结构类型节点
-    参数 : (输出)数据结构节点
-    返回 : (成功)0
-           (失败)-1
-\*========================================*/
-int fxmlStcCreate(struct txmlItem **item)
-{
-	int result;
-	result=fxmlInit(item);
-	if(result!=0)
-		return -1;
-	(*item)->type=cxmlStc;
-	return 0;
-}
-
-/*========================================*\
-    功能 : 插入OBJ类型节点
-    参数 : (输入)对象节点
+    功能 : 插入节点
+    参数 : (输入)目标节点
            (输入)插入节点
-           (输入)键数据
-           (输入)键长度
+           (输入)类型
     返回 : 空
 \*========================================*/
-void fxmlObjInsert(struct txmlItem *obj,struct txmlItem *item,char *keyd,int keyl)
+void fxmlInsert(struct txmlItem *target,struct txmlItem *insert,int type)
 {
-	item->keyd=keyd;
-	item->keyl=keyl;
 	struct txmlItem *temp;
-	temp=obj->chld;
+	if(type==cxmlNode)
+		temp=target->nodechld;
+	else
+	if(type==cxmlAttr)
+		temp=target->attrchld;
 	if(temp==NULL)
 	{
-		obj->chld=item;
+		if(type==cxmlNode)
+			target->nodechld=insert;
+		else
+		if(type==cxmlAttr)
+			target->attrchld=insert;
 		return;
 	}
 	while(1)
@@ -126,362 +115,194 @@ void fxmlObjInsert(struct txmlItem *obj,struct txmlItem *item,char *keyd,int key
 			break;
 		temp=temp->next;
 	}
-	temp->next=item;
+	temp->next=insert;
 }
 
 /*========================================*\
-    功能 : 导出KEY类型节点
-    参数 : (输入)数据结构节点
-           (输出)起始结束位置
-    返回 : 空
-\*========================================*/
-void fxmlKeyExport(struct txmlItem *item,char **dst)
-{
-	sprintf(*dst,"\"%.*s\"",item->keyl,item->keyd);
-	*dst+=item->keyl+2;
-}
-
-/*========================================*\
-    功能 : 导出STR类型节点
-    参数 : (输入)数据结构节点
-           (输出)起始结束位置
-    返回 : 空
-\*========================================*/
-void fxmlStrExport(struct txmlItem *item,char **dst)
-{
-	sprintf(*dst,"\"%.*s\"",item->vall,item->vald);
-	*dst+=item->vall+2;
-}
-
-/*========================================*\
-    功能 : 导出OBJ类型节点
-    参数 : (输入)数据结构节点
-           (输出)起始结束位置
+    功能 : 查询节点
+    参数 : (输入)目标节点
+           (输出)查询节点
+           (输入)类型
+           (输入)键数据
+           (输入)键长度
+           (输入)序号
     返回 : (成功)0
            (失败)-1
 \*========================================*/
-int fxmlObjExport(struct txmlItem *item,char **dst)
+int fxmlSelect(struct txmlItem *target,struct txmlItem **select,int type,char *keyd,int keyl,int index)
+{
+	if(type==cxmlNode)
+		*select=target->nodechld;
+	else
+	if(type==cxmlAttr)
+		*select=target->attrchld;
+	while(1)
+	{
+		if(*select==NULL)
+			return -1;
+		if(strncmp((*select)->keyd,keyd,keyl)==0&&(*select)->keyl==keyl)
+			if(index==0)
+				return 0;
+			else
+				index--;
+		*select=(*select)->next;
+	}
+}
+
+/*========================================*\
+    功能 : 导出ATTR类型节点
+    参数 : (输入)节点
+           (出入)位置
+    返回 : 空
+\*========================================*/
+void fxmlAttrExport(struct txmlItem *item,char **dst)
+{
+	*dst+=sprintf(*dst,"%.*s=\"%.*s\"",item->keyl,item->keyd,item->vall,item->vald);
+}
+
+/*========================================*\
+    功能 : 导出NODE类型节点
+    参数 : (输入)节点
+           (出入)位置
+    返回 : (成功)0
+           (失败)-1
+\*========================================*/
+int fxmlNodeExport(struct txmlItem *item,char **dst)
 {
 	int result;
-
+	*(*dst)++='<';
+	*dst+=sprintf(*dst,"%.*s",item->keyl,item->keyd);
 	struct txmlItem *current;
-	current=item->chld;
-	*(*dst)++='{';
+	current=item->attrchld;
 	while(1)
 	{
 		if(current==NULL)
 			break;
-		fxmlKeyExport(current,dst);
-		*(*dst)++=':';
-		result=fxmlAllExport(current,dst);
+		*(*dst)++=' ';
+		fxmlAttrExport(current,dst);
+		current=current->next;
+	}
+	*(*dst)++='>';
+	*dst+=sprintf(*dst,"%.*s",item->vall,item->vald);
+	current=item->nodechld;
+	while(1)
+	{
+		if(current==NULL)
+			break;
+		result=fxmlNodeExport(current,dst);
 		if(result!=0)
 			return -1;
 		current=current->next;
-		if(current!=NULL)
-			*(*dst)++=',';
 	}
-	*(*dst)++='}';
-
+	*(*dst)++='<';
+	*(*dst)++='/';
+	*dst+=sprintf(*dst,"%.*s",item->keyl,item->keyd);
+	*(*dst)++='>';
 	return 0;
 }
 
 /*========================================*\
-    功能 : 导出所有类型节点
-    参数 : (输入)数据结构节点
-           (输出)起始结束位置
+    功能 : 导入ATTR类型节点
+    参数 : (输出)节点
+           (出入)位置
     返回 : (成功)0
            (失败)-1
 \*========================================*/
-int fxmlAllExport(struct txmlItem *item,char **dst)
+int fxmlAttrImport(struct txmlItem **item,char **src)
 {
 	int result;
 
-	switch(item->type)
-	{
-		case cxmlArr:
-		result=fxmlArrExport(item,dst);
-		if(result!=0)
-			return -1;
-		break;
-
-		case cxmlObj:
-		result=fxmlObjExport(item,dst);
-		if(result!=0)
-			return -1;
-		break;
-
-		case cxmlStr:
-		fxmlStrExport(item,dst);
-		break;
-
-		case cxmlNum:
-		fxmlNumExport(item,dst);
-		break;
-	}
-
-	return 0;
-}
-
-/*========================================*\
-    功能 : 导入KEY类型节点
-    参数 : (输入)数据结构节点
-           (出入)起始结束位置
-    返回 : 空
-\*========================================*/
-void fxmlKeyImport(struct txmlItem *item,char **src)
-{
-	item->keyd=*src+1;
-
-	(*src)++;
-	while(1)
-	{
-		int escape;
-		if(**src=='\\')
-			escape=1;
-		else
-			escape=0;
-
-		if(**src=='\"'&&escape==0)
-			break;
-
-		(*src)++;
-
-		item->keyl++;
-	}
-	(*src)++;
-}
-/*========================================*\
-    功能 : 导入STR类型节点
-    参数 : (输入)数据结构节点
-           (出入)起始结束位置
-    返回 : (成功)0
-           (失败)-1
-\*========================================*/
-int fxmlStrImport(struct txmlItem *item,char **src)
-{
-	char *record;
-	record=*src+1;
-
-	(*src)++;
-	while(1)
-	{
-		int escape;
-		if(**src=='\\')
-			escape=1;
-		else
-			escape=0;
-
-		if(**src=='\"'&&escape==0)
-			break;
-
-		(*src)++;
-
-		item->vall++;
-	}
-	(*src)++;
-
-	item->vald=(char*)malloc(item->vall);
-	if(item->vald==NULL)
+	result=fxmlInit(item);
+	if(result!=0)
 		return -1;
-	memcpy(item->vald,record,item->vall);
-
-	return 0;
-}
-
-/*========================================*\
-    功能 : 导入NUM类型节点
-    参数 : (输入)数据结构节点
-           (出入)起始结束位置
-    返回 : (成功)0
-           (失败)-1
-\*========================================*/
-int fxmlNumImport(struct txmlItem *item,char **src)
-{
-	char *record;
-	record=*src;
-	strtod(*src,src);
-	item->vall=*src-record;
-	item->vald=(char*)malloc(item->vall);
-	if(item->vald==NULL)
+	(*item)->keyl=mxmlSize(src,cxmlSpace"=");
+	(*item)->keyd=(char*)malloc((*item)->keyl);
+	if((*item)->keyd==NULL)
 		return -1;
-	memcpy(item->vald,record,item->vall);
+	memcpy((*item)->keyd,*src,(*item)->keyl);
+	*src+=(*item)->keyl;
+	mxmlSkip(src,cxmlSpace);
+	if(*(*src)++!='=')
+		return -1;
+	mxmlSkip(src,cxmlSpace);
+	if(*(*src)++!='\"')
+		return -1;
+	(*item)->vall=mxmlSize(src,"\"");
+	(*item)->vald=(char*)malloc((*item)->vall);
+	if((*item)->vald==NULL)
+		return -1;
+	memcpy((*item)->vald,*src,(*item)->vall);
+	*src+=(*item)->vall;
+	(*src)++;
+
 	return 0;
 }
 
 /*========================================*\
-    功能 : 导入ARR类型节点
-    参数 : (输入)数据结构节点
-           (出入)起始结束位置
+    功能 : 导入NODE类型节点
+    参数 : (输出)节点
+           (出入)位置
     返回 : (成功)0
            (失败)-1
 \*========================================*/
-int fxmlArrImport(struct txmlItem *item,char **src)
+int fxmlNodeImport(struct txmlItem **item,char **src)
 {
 	int result;
 
-	(*src)++;
+	if(*(*src)++!='<')
+		return -1;
+	result=fxmlInit(item);
+	if(result!=0)
+		return -1;
+	(*item)->keyl=mxmlSize(src,cxmlSpace">");
+	(*item)->keyd=(char*)malloc((*item)->keyl);
+	if((*item)->keyd==NULL)
+		return -1;
+	memcpy((*item)->keyd,*src,(*item)->keyl);
+	*src+=(*item)->keyl;
 	while(1)
 	{
-		mxmlSkip(src);
-		if(**src==']')
+		mxmlSkip(src,cxmlSpace);
+		if(**src=='>')
 			break;
-		struct txmlItem *current;
-		result=fxmlInit(&current);
+		struct txmlItem *temp;
+		result=fxmlAttrImport(&temp,src);
 		if(result!=0)
 			return -1;
-		current->next=item->chld;
-		item->chld=current;
-		mxmlSkip(src);
-		result=fxmlAllImport(current,src);
-		if(result!=0)
-			return -1;
-		mxmlSkip(src);
-		if(**src!=','&&**src!=']')
-			return -1;
-		if(**src==',')
-			(*src)++;
+		fxmlInsert((*item),temp,cxmlAttr);
 	}
-	(*src)++;
-
-	return 0;
-}
-
-/*========================================*\
-    功能 : 导入OBJ类型节点
-    参数 : (输入)数据结构节点
-           (出入)起始结束位置
-    返回 : (成功)0
-           (失败)-1
-\*========================================*/
-int fxmlObjImport(struct txmlItem *item,char **src)
-{
-	int result;
-
 	(*src)++;
 	while(1)
 	{
-		mxmlSkip(src);
-		if(**src=='}')
+		mxmlSkip(src,cxmlSpace);
+		if(**src=='<'&&*(*src+1)=='/')
 			break;
-		struct txmlItem *current;
-		result=fxmlInit(&current);
+		if(**src!='<')
+		{
+			(*item)->vall=mxmlSize(src,"<");
+			(*item)->vald=(char*)malloc((*item)->vall);
+			if((*item)->vald==NULL)
+				return -1;
+			memcpy((*item)->vald,*src,(*item)->vall);
+			*src+=(*item)->vall;
+			continue;
+		}
+		struct txmlItem *temp;
+		result=fxmlNodeImport(&temp,src);
 		if(result!=0)
 			return -1;
-		current->next=item->chld;
-		item->chld=current;
-		mxmlSkip(src);
-		fxmlKeyImport(current,src);
-		mxmlSkip(src);
-		if(*(*src)++!=':')
-			return -1;
-		mxmlSkip(src);
-		result=fxmlAllImport(current,src);
-		if(result!=0)
-			return -1;
-		mxmlSkip(src);
-		if(**src!=','&&**src!='}')
-			return -1;
-		if(**src==',')
-			(*src)++;
+		fxmlInsert((*item),temp,cxmlNode);
 	}
-	(*src)++;
+	(*src)+=(*item)->keyl;
+	(*src)+=3;
 
 	return 0;
 }
 
 /*========================================*\
-    功能 : 导入所有类型节点
-    参数 : (输入)数据结构节点
-           (出入)起始结束位置
-    返回 : (成功)0
-           (失败)-1
-\*========================================*/
-int fxmlAllImport(struct txmlItem *item,char **src)
-{
-	int result;
-
-	switch(**src)
-	{
-		case '[':
-		item->type=cxmlArr;
-		result=fxmlArrImport(item,src);
-		if(result!=0)
-			return -1;
-		break;
-
-		case '{':
-		item->type=cxmlObj;
-		result=fxmlObjImport(item,src);
-		if(result!=0)
-			return -1;
-		break;
-
-		case '\"':
-		item->type=cxmlStr;
-		result=fxmlStrImport(item,src);
-		if(result!=0)
-			return -1;
-		break;
-
-		default:
-		item->type=cxmlNum;
-		result=fxmlNumImport(item,src);
-		if(result!=0)
-			return -1;
-		break;
-	}
-
-	return 0;
-}
-
-/*========================================*\
-    功能 : 查询OBJ类型节点
-    参数 : (输入)数组节点
-           (输出)查询节点
-           (输入)序
-    返回 : (成功)0
-           (失败)-1
-\*========================================*/
-int fxmlArrSelect(struct txmlItem *arr,struct txmlItem **item,int ind)
-{
-	*item=arr->chld;
-	while(1)
-	{
-		if(*item==NULL)
-			return -1;
-		if(ind==0)
-			return 0;
-		*item=(*item)->next;
-		ind--;
-	}
-}
-
-/*========================================*\
-    功能 : 查询OBJ类型节点
-    参数 : (输入)对象节点
-           (输出)查询节点
-           (输入)键数据
-           (输入)键长度
-    返回 : (成功)0
-           (失败)-1
-\*========================================*/
-int fxmlObjSelect(struct txmlItem *obj,struct txmlItem **item,char *keyd,int keyl)
-{
-	*item=obj->chld;
-	while(1)
-	{
-		if(*item==NULL)
-			return -1;
-		if(strncmp((*item)->keyd,keyd,keyl)==0&&(*item)->keyl==keyl)
-			return 0;
-		*item=(*item)->next;
-	}
-}
-
-/*========================================*\
-    功能 : 将数据结构导出到报文
-    参数 : (输入)数据结构节点
-           (输出)报文数据
+    功能 : 将节点导出到报文
+    参数 : (输入)节点
+           (出入)报文数据
            (输出)报文长度
     返回 : (成功)0
            (失败)-1
@@ -491,7 +312,7 @@ int fxmlExport(struct txmlItem *item,char **pd,int *pl)
 	char *record;
 	record=*pd;
 	int result;
-	result=fxmlAllExport(item,pd);
+	result=fxmlNodeExport(item,pd);
 	if(result!=0)
 		return -1;
 	*pl=*pd-record;
@@ -499,21 +320,23 @@ int fxmlExport(struct txmlItem *item,char **pd,int *pl)
 }
 
 /*========================================*\
-    功能 : 将报文导入到数据结构
-    参数 : (输出)数据结构节点
+    功能 : 将报文导入到节点
+    参数 : (输出)节点
            (出入)报文数据
            (输入)报文长度
     返回 : (成功)0
            (失败)-1
 \*========================================*/
-int fxmlImport(struct txmlItem *item,char **pd,int pl)
+int fxmlImport(struct txmlItem **item,char **pd,int pl)
 {
 	char *record;
 	record=*pd;
+	mxmlSkip(pd,cxmlSpace);
 	int result;
-	result=fxmlAllImport(item,pd);
+	result=fxmlNodeImport(item,pd);
 	if(result!=0)
 		return -1;
+	mxmlSkip(pd,cxmlSpace);
 	if(*pd-record!=pl)
 		return -1;
 	return 0;
